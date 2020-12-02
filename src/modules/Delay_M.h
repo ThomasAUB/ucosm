97852ee539eedcc96c60825c1ae22824ccd2d031
ThomasAUB <aubert.thms@gmail.com>
2020-12-02 21:19:23 +0100

    add Delay_M module

---
+++ src/modules/Delay_M.h
@@ -0,0 +1,41 @@
+#pragma once
+
+#include "ucosm-sys-data.h"
+
+
+struct Delay_M
+{
+
+	void setDelay(tick_t inDelay){
+		mExecution_time_stamp = SysKernelData::sGetTick()+inDelay; 
+	}
+	
+	tick_t getDelay(){
+		if(mExecution_time_stamp > SysKernelData::sGetTick()){
+			return mExecution_time_stamp - SysKernelData::sGetTick(); 
+		}else{
+			return 0;
+		}
+	}
+
+	void init(){
+		mExecution_time_stamp = SysKernelData::sGetTick();
+	}
+
+	bool isExeReady() const {
+		return (SysKernelData::sGetTick() >= mExecution_time_stamp);
+	}
+
+	bool isDelReady() const { return true; }
+
+	void makePreExe(){}
+
+	void makePreDel(){}
+
+	void makePostExe(){}
+		
+private:
+	
+	tick_t mExecution_time_stamp;
+};
+
