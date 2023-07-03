72bca9d736bcb88a89ffc90c03469b1d5d1eba8a
Thomas AUBERT <thomas.aubert@arturia.com>
2023-07-03 18:15:12 +0200

    fix multi instance task_function

--- include/ucosm/task_function.h
+++ include/ucosm/task_function.h
@@ -45,13 +45,11 @@ class TaskFunction: public ITask {
 
     static_assert(task_count > 0, "Task count must be at least 1");
 
-    struct TaskItem: public module_M {
-        constexpr TaskItem() :
-                index(sCounterIndex++) {
-        }
-        const task_index_t index;
-    private:
-        static task_index_t sCounterIndex;
+    class TaskItem: public module_M {
+        task_index_t mIndex;
+        friend class TaskFunction<caller_t, task_count, module_M>;
+    public:
+        task_index_t index() const { return mIndex; }
     };
 
     enum throwExcept {
@@ -67,9 +65,8 @@ public:
 
     struct TaskHandle {
 
-        TaskHandle() :
-                mP(nullptr) {
-        }
+        TaskHandle() = default;
+
         ~TaskHandle() {
             mP = nullptr;
         }
@@ -95,20 +92,15 @@ public:
         }
 
     private:
-        TaskItem *mP;
-        static TaskFunction<caller_t, task_count, module_M> *handler;
+        TaskItem *mP = nullptr;
+        static inline TaskFunction<caller_t, task_count, module_M> *handler = nullptr;
         friend class TaskFunction<caller_t, task_count, module_M> ;
     };
 
-    TaskFunction() :
-            mFunctions { nullptr }, mHandlePtr { nullptr }, mActiveTaskCount(0) {
+    TaskFunction() {
 
         for (uint32_t i = 0; i < task_count; i++) {
-            if (mTasks[i].index != i) {
-                // something went wrong,
-                // you may have declared several instances on the same type
-                while (1) {}
-            }
+            mTasks[i].mIndex = i; 
         }
 
         TaskHandle::handler = this;
@@ -154,28 +146,21 @@ private:
     void allocate(task_function_t inFunc, task_index_t i, TaskHandle *ioHandle);
 
     // task's module(s) and index
-    TaskItem mTasks[task_count];
+    TaskItem mTasks[task_count] {};
 
     // task function pointers
-    task_function_t mFunctions[task_count];
+    task_function_t mFunctions[task_count] {};
 
     // task handle pointers
-    TaskHandle *mHandlePtr[task_count];
+    TaskHandle *mHandlePtr[task_count] {};
 
     // count of currently active tasks
-    task_index_t mActiveTaskCount;
+    task_index_t mActiveTaskCount = 0;
 
     // currently scheduled task
     TaskHandle mCurTask;
 };
 
-template<typename caller_t, uint32_t task_count, typename module_M>
-typename TaskFunction<caller_t, task_count, module_M>::task_index_t TaskFunction<
-        caller_t, task_count, module_M>::TaskItem::sCounterIndex = 0;
-
-template<typename caller_t, uint32_t task_count, typename module_M>
-TaskFunction<caller_t, task_count, module_M> *TaskFunction<caller_t, task_count,
-        module_M>::TaskHandle::handler = nullptr;
 
 template<typename caller_t, uint32_t task_count, typename module_M>
 bool TaskFunction<caller_t, task_count, module_M>::schedule() {
@@ -256,7 +241,7 @@ bool TaskFunction<caller_t, task_count, module_M>::deleteTask(
         return false;
     }
 
-    task_index_t i = inHandle.mP->index;
+    task_index_t i = inHandle.mP->index();
 
     // check if the task is ready to be deleted
     if (mFunctions[i] && mTasks[i].isDelReady()) {
@@ -305,7 +290,7 @@ TaskFunction<caller_t, task_count, module_M>::getTaskFunction(TaskHandle inHandl
     if (!inHandle()) {
         return 0;
     }
-    return mFunctions[inHandle.mP->index];
+    return mFunctions[inHandle.mP->index()];
 }
 
 // returns the count of currently active tasks
